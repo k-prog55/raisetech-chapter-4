85a3b53640ed1c638f9f960f303d176dd917c590
k-prog55 <keianzai@keinoMacBook-Air.local>
2023-07-09 21:46:34 +0900

    lesson4-1 commit

---
+++ src/Main2.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args){
+    List<String> names = List.of("yamada","tanaka","yasuda","suzuki","iida");
+    List<String> result = new ArrayList<>();
+
+    for(int i = 0; i < names.size(); i++) {
+        if (names.get(i).contains("a")) {
+            result.add(names.get(i));
+        }
+    }
+        System.out.println(result);
+    }
+
+    for(String name : names){
+        if (name.contains("a")){
+            result.add(name);
+        }
+    }
+    
+}
